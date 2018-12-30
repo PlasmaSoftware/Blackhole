32f33349f7d6d2db79770e599e060ed3a4e2acd0
austinv11 <austinv11@gmail.com>
2018-12-30 09:03:16 -0500

    FieldGetter on Java 8 should be more efficient on invocations

--- src/main/java/plasma/blackhole/util/support/FieldGetter.java
+++ src/main/java/plasma/blackhole/util/support/FieldGetter.java
@@ -1,6 +1,9 @@
 package plasma.blackhole.util.support;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.function.Function;
 
 // Java 8 field getter
@@ -9,13 +12,18 @@ public class FieldGetter {
     public static <O, T> Function<O, T> makeGetter(Class<O> clazz, String fieldName, Class<T> fieldType) {
         return new Function<O, T>() {
 
-            private final Field f;
+            private final MethodHandle fg;
+            private final boolean isStatic;
 
             {
                 try {
-                    f = clazz.getField(fieldName);
-                    f.setAccessible(true);
-                } catch (NoSuchFieldException e) {
+                    Field f = clazz.getField(fieldName);
+                    MethodHandles.Lookup lookup = MethodHandles.lookup().in(clazz);
+                    isStatic = Modifier.isStatic(f.getModifiers());
+                    fg = isStatic
+                            ? lookup.findStaticGetter(f.getDeclaringClass(), fieldName, fieldType)
+                            : lookup.findGetter(f.getDeclaringClass(), fieldName, fieldType);
+                } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             }
@@ -23,9 +31,9 @@ public class FieldGetter {
             @Override
             public T apply(O obj) {
                 try {
-                    return (T) f.get(obj);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
+                    return isStatic ? (T) fg.invoke() : (T) fg.bindTo(obj).invoke();
+                } catch (Throwable t) {
+                    throw new RuntimeException(t);
                 }
             }
         };
