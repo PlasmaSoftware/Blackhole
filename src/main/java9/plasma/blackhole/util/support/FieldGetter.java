75df0d63a62c0ce486f187adb3c333ca089b8647
austinv11 <austinv11@gmail.com>
2019-01-11 09:35:52 -0500

    Handle static fields in varhandles better

--- src/main/java9/plasma/blackhole/util/support/FieldGetter.java
+++ src/main/java9/plasma/blackhole/util/support/FieldGetter.java
@@ -2,6 +2,8 @@ package plasma.blackhole.util.support;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.function.Function;
 
 //Java 9+ field getter
@@ -14,8 +16,10 @@ public class FieldGetter {
 
             {
                 try {
-                    f = MethodHandles.privateLookupIn(clazz, MethodHandles.lookup())
-                            .findVarHandle(clazz, fieldName, fieldType);
+                    Field field = clazz.getDeclaredField(fieldName);
+                    field.setAccessible(true);
+                    MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(clazz, MethodHandles.lookup());
+                    f = lookup.unreflectVarHandle(field);
                 } catch (NoSuchFieldException | IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
